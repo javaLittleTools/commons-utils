1a06ae80acb1cb31f29e1e3cd77690ff069c2c4d
Piggy4SxL <tulip_sxl@outlook.com>
2018-12-21 12:49:12 +0800

    [UPDATED] remove QrCodeArgs

--- src/main/java/cn/sxl/utils/qrcode/QrCodeUtils.java
+++ src/main/java/cn/sxl/utils/qrcode/QrCodeUtils.java
@@ -1,11 +1,13 @@
 package cn.sxl.utils.qrcode;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * 生成二维码
@@ -21,7 +23,7 @@ public class QrCodeUtils {
      * @param response 响应体
      * @return 二维码生成图
      */
-    public static BitMatrix generateQRCode(String content, HttpServletResponse response, QrCodeArgs qrCodeArgs){
+    public static BitMatrix generateQRCode(String content, HttpServletResponse response, int width, int height, Map<EncodeHintType, Object> hints){
         response.setHeader("Cache-Control", "no-store");
         response.setHeader("Pragma", "no-cache");
         response.setDateHeader("Expires", 0);
@@ -31,7 +33,25 @@ public class QrCodeUtils {
         try {
             // 编码内容，编码类型，宽度，高度，设置参数
             bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE,
-                    qrCodeArgs.getWidth(), qrCodeArgs.getHeight(), qrCodeArgs.getHints());
+                    width, height, hints);
+        } catch (WriterException e) {
+            return null;
+        }
+
+        return bitMatrix;
+    }
+
+    public static BitMatrix generateQRCode(String content, HttpServletResponse response, Map<EncodeHintType, Object> hints){
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/png");
+        BitMatrix bitMatrix;
+
+        try {
+            // 编码内容，编码类型，宽度，高度，设置参数
+            bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE,
+                    200, 200, hints);
         } catch (WriterException e) {
             return null;
         }
